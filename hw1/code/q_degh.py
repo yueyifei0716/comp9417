8984bea99f92156246dd0937e96c89ba5f071b93
Yifei YUE <vincentyueyifei@gmail.com>
2022-06-26 16:41:33 +0800

    Updating sols for k

--- hw1/code/q_degh.py
+++ hw1/code/q_degh.py
@@ -3,9 +3,10 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import copy
 
 
-#### d
+### Code for Question d
 data = pd.read_csv('CarSeats.csv')
 
 label = ['Sales']
@@ -46,7 +47,7 @@ print(Y_test[-1])
 print()
 
 
-##### e
+### Code for Question e
 phi = 0.5
 size = len(data.columns) - len(label + categorical_features)
 X_t = X_train.T
@@ -57,10 +58,8 @@ print('The value of the ridge solution based on X_train and Y_train:\n')
 print(beta_hat)
 
 
-#### g
-
+### Code for Question g
 beta_0 = np.ones(X_train.shape[1]).reshape(7, 1)
-# X_train_t = X_train.T
 
 phi = 0.5
 epochs = 1000
@@ -74,7 +73,7 @@ def grad(X_train, Y_train, beta, phi):
     grad_beta = -2 * (1/len(X_train)) * X_train.T @ (Y_train - X_train @ beta) + 2 * phi * beta
     return grad_beta
 
-def get_betas(beta_0, X_train, Y_train, alpha, phi):
+def get_gd_betas(beta_0, X_train, Y_train, alpha, phi, epochs):
     beta = beta_0
     betas = []
     betas.append(beta)
@@ -84,9 +83,9 @@ def get_betas(beta_0, X_train, Y_train, alpha, phi):
         betas.append(b_updated)
     return betas
 
-def get_deltas(beta_0, X_train, Y_train, alpha, phi):
+def get_gd_deltas(beta_0, X_train, Y_train, alpha, phi, epochs):
     deltas = []
-    betas = get_betas(beta_0, X_train, Y_train, alpha, phi)
+    betas = get_gd_betas(beta_0, X_train, Y_train, alpha, phi, epochs)
     loss_hat = loss(X_train, Y_train, beta_hat, phi)
     for beta in betas:
         delta = loss(X_train, Y_train, beta, phi) - loss_hat
@@ -95,18 +94,19 @@ def get_deltas(beta_0, X_train, Y_train, alpha, phi):
 
 i = 1
 for alpha in alphas:
-    deltas = get_deltas(beta_0, X_train, Y_train, alpha, phi)
+    deltas = get_gd_deltas(beta_0, X_train, Y_train, alpha, phi, epochs)
     axes = plt.subplot(3, 3, i)
     plt.plot(range(epochs + 1), deltas, color='blue')
     plt.xlabel('k')
     plt.ylabel('delta')
     plt.title('alpha = ' + str(np.format_float_positional(alpha)))
     i = i + 1
+plt.suptitle('Batch GD')
 plt.tight_layout()
-plt.savefig("GB plot.png", dpi=400)
-# plt.show()
+plt.savefig('GD_plot.png', dpi=400)
+plt.show()
 
-beta_best = get_betas(beta_0, X_train, Y_train, 0.005, phi)[-1]
+beta_best = get_gd_betas(beta_0, X_train, Y_train, 0.005, phi, epochs)[-1]
 
 train_MSE = 1/len(X_train) * (np.linalg.norm(Y_train - X_train @ beta_best) ** 2)
 print('The train MSE is :', train_MSE)
@@ -115,16 +115,13 @@ test_MSE = 1/len(X_train) * (np.linalg.norm(Y_test - X_test @ beta_best) ** 2)
 print('The test MSE is :', test_MSE)
 
 
-#### h
-
-# #########################################################################
-
+### Code for Question h
 beta_0 = np.ones(X_train.shape[1]).reshape(7, 1)
 phi = 0.5
 epochs = 5
 sgd_alphas = [0.000001, 0.000005, 0.00001, 0.00005, 0.0001, 0.0005, 0.001, 0.006, 0.02]
 
-def get_sgd_betas(beta_0, X_train, Y_train, alpha, phi):
+def get_sgd_betas(beta_0, X_train, Y_train, alpha, phi, epochs):
     beta = beta_0
     betas = []
     betas.append(beta)
@@ -139,9 +136,9 @@ def get_sgd_betas(beta_0, X_train, Y_train, alpha, phi):
             betas.append(b_updated)
     return betas
 
-def get_sgd_deltas(beta_0, X_train, Y_train, alpha, phi):
+def get_sgd_deltas(beta_0, X_train, Y_train, alpha, phi, epochs):
     deltas = []
-    betas = get_sgd_betas(beta_0, X_train,Y_train, alpha, phi)
+    betas = get_sgd_betas(beta_0, X_train,Y_train, alpha, phi, epochs)
     loss_hat = loss(X_train, Y_train, beta_hat, phi)
     for beta in betas:
         delta = loss(X_train, Y_train, beta, phi) - loss_hat
@@ -150,19 +147,19 @@ def get_sgd_deltas(beta_0, X_train, Y_train, alpha, phi):
 
 i = 1
 for alpha in sgd_alphas:
-    deltas = get_sgd_deltas(beta_0, X_train, Y_train, alpha, phi)
+    deltas = get_sgd_deltas(beta_0, X_train, Y_train, alpha, phi, epochs)
     axes = plt.subplot(3, 3, i)
     plt.plot(range(epochs * len(X_train) + 1), deltas, color='blue')
     plt.xlabel('k')
     plt.ylabel('delta')
     plt.title('alpha = ' + str(np.format_float_positional(alpha)))
     i = i + 1
+plt.suptitle('SGD')
 plt.tight_layout()
-plt.savefig("SGB plot.png", dpi=400)
-# plt.show()
+plt.savefig('SGD_plot.png', dpi=400)
+plt.show()
 
-
-beta_best = get_sgd_betas(beta_0, X_train, Y_train, 0.006, phi)[-1]
+beta_best = get_sgd_betas(beta_0, X_train, Y_train, 0.006, phi, epochs)[-1]
 
 train_MSE = 1/len(X_train) * (np.linalg.norm(Y_train - X_train @ beta_best) ** 2)
 print('The train MSE is :', train_MSE)
@@ -171,17 +168,56 @@ test_MSE = 1/len(X_train) * (np.linalg.norm(Y_test - X_test @ beta_best) ** 2)
 print('The test MSE is :', test_MSE)
 
 
-# ### j: 全新的更新方式，每一轮只更新一个weight，假设有p个weight w(w1 w2 ... wp)， 1:03:05
+### Code for Question k
+cycles = 10
+p = len(X_train[0])
+beta_0 = np.ones(X_train.shape[1]).reshape(7, 1)
+phi = 0.5
 
 
-# ### k: 实现j问算法 grad的方程
+def get_new_betas(beta_0, X_train, Y_train, phi):
+    beta = beta_0
+    betas = []
+    betas.append(beta)
+    y = Y_train
+    for i in range(cycles):
+        for j in range(p):
+            x_j = X_train[:, j].reshape(1, len(X_train))
+            x_nj = np.delete(X_train, j, 1)
+            beta_nj = np.delete(beta, j, 0)
+            bj_updated = (x_j @ y - x_j @ x_nj @ beta_nj) / (x_j @ x_j.T + len(X_train) * phi)
+            beta = copy.deepcopy(beta)
+            beta[j] = bj_updated
+            betas.append(beta)
+    return betas
 
-# p = size        # size = the number of remaining features = 7
-# b = b0
-# for i in range(10):
-#     for j in range(p):
-#         # grad = 2/len(X_train) * ()
-#         b_updated = (-2 * (1/len(X_train)) * x_t @ (y - x @ b) + 2 * phi * b)
+
+def get_new_deltas(beta_0, X_train, Y_train, phi):
+    deltas = []
+    betas = get_new_betas(beta_0, X_train,Y_train, phi)
+    loss_hat = loss(X_train, Y_train, beta_hat, phi)
+    for beta in betas:
+        delta = loss(X_train, Y_train, beta, phi) - loss_hat
+        deltas.append(delta)
+    return deltas
 
 
+deltas = get_new_deltas(beta_0, X_train, Y_train, phi)
 
+plt.plot(range(cycles * p + 1), deltas, color='blue', label='new algorithm')
+plt.plot(range(cycles * p + 1), get_gd_deltas(beta_0, X_train, Y_train, 0.005, phi, 1000)[0:cycles * p + 1], color='green', label='batch GD')
+plt.plot(range(cycles * p + 1), get_sgd_deltas(beta_0, X_train, Y_train, 0.006, phi, 5)[0:cycles * p + 1], color='orange', label='SGD')
+plt.xlabel('k')
+plt.legend(loc='upper right')                           # creates legend in top right corner of plot
+plt.ylabel('delta')
+plt.title('New algorithem compared with GD and SGD')
+plt.savefig('comparison.png', dpi=400)
+plt.show()
+
+beta_best = get_new_betas(beta_0, X_train, Y_train, phi)[-1]
+
+train_MSE = 1/len(X_train) * (np.linalg.norm(Y_train - X_train @ beta_best) ** 2)
+print('The train MSE is :', train_MSE)
+
+test_MSE = 1/len(X_train) * (np.linalg.norm(Y_test - X_test @ beta_best) ** 2)
+print('The test MSE is :', test_MSE)
